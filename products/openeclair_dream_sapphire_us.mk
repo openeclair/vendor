910f422f1d7a7d559abf87c27cabf15b0decaf2c
Chris <k@k.com>
2010-02-02 01:22:57 +0000

    I dont remember what I changed...

--- products/openeclair_dream_sapphire_us.mk
+++ products/openeclair_dream_sapphire_us.mk
@@ -17,6 +17,7 @@
 # This is the top-level configuration for a US-configured openeclairMod build
 
 $(call inherit-product, vendor/aosp/products/aosp_dream_us.mk)
+#$(call inherit-product, vendor/aosp/products/aosp_sapphire_us.mk)
 
 USE_CAMERA_STUB := false
 
