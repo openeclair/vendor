67463211bebfdda757937d8ea01af2fbb89b1e6d
Wes Garner <wesgarner@gmail.com>
2010-02-12 21:43:48 -0600

    added HTC_IME

--- products/openeclair_dream_sapphire_us.mk
+++ products/openeclair_dream_sapphire_us.mk
@@ -72,6 +72,8 @@ PRODUCT_PACKAGES += Superuser \
 PRODUCT_PACKAGE_OVERLAYS := vendor/openeclair/overlay
 
 # APKs
+PRODUCT_COPY_FILES += vendor/openeclair/proprietary/HTC_IME.apk:data/app_s_tmp/HTC_IME.apk
+PRODUCT_COPY_FILES += vendor/openeclair/proprietary/Clicker.apk:data/app_s_tmp/Clicker.apk
 PRODUCT_COPY_FILES += vendor/openeclair/proprietary/Vending.apk:data/app_s_tmp/Vending.apk
 PRODUCT_COPY_FILES += vendor/openeclair/proprietary/VoiceSearchWithKeyboard.apk:data/app_s_tmp/VoiceSearchWithKeyboard.apk
 PRODUCT_COPY_FILES += vendor/openeclair/proprietary/Maps.apk:data/app_s_tmp/Maps.apk
