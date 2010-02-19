ee097467761f1ff04659b2cdbbcf82d72c518420
Wes Garner <wesgarner@gmail.com>
2010-02-19 00:56:39 -0600

    added default properties for memctl

--- products/openeclair_dream_sapphire_us.mk
+++ products/openeclair_dream_sapphire_us.mk
@@ -30,8 +30,9 @@ ADDITIONAL_BUILD_PROPERTIES += ro.com.android.dataroaming=true
 ADDITIONAL_BUILD_PROPERTIES += ro.ril.hsxpa=2
 ADDITIONAL_BUILD_PROPERTIES += ro.ril.gprsclass=10
 ADDITIONAL_BUILD_PROPERTIES += ro.com.google.clientidbase=android-tmobile
-
-ADDITIONAL_BUILD_PROPERTIES += ro.modversion=OpenEclair-v1.2.1
+ADDITIONAL_BUILD_PROPERTIES += ro.modversion=OpenEclair-v1.2.2-B2
+ADDITIONAL_DEFAULT_PROPERTIES += persist.service.memctl_state=1
+ADDITIONAL_DEFAULT_PROPERTIES += persist.service.memctl_size=16384
 
 USE_CAMERA_STUB := false
 
