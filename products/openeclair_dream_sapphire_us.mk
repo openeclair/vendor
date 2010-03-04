5c98ff75d87ac4e23e6f87c6547ee145928fc539
Wes Garner <wesgarner@gmail.com>
2010-03-03 19:30:24 -0600

    changing these properties only henders data connection

--- products/openeclair_dream_sapphire_us.mk
+++ products/openeclair_dream_sapphire_us.mk
@@ -24,8 +24,6 @@ TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/../prebuilt/dream-sapphire/zImage
 ADDITIONAL_BUILD_PROPERTIES += ro.com.google.locationfeatures=1
 ADDITIONAL_BUILD_PROPERTIES += ro.url.legal=http://www.google.com/intl/%s/mobile/android/android-dev-phone-legal.html
 ADDITIONAL_BUILD_PROPERTIES += ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/android-dev-phone-privacy.html
-PRODUCT_PROPERTY_OVERRIDES += ro.ril.hsxpa=2
-PRODUCT_PROPERTY_OVERRIDES += ro.ril.gprsclass=10
 ADDITIONAL_BUILD_PROPERTIES += ro.com.google.clientidbase=android-tmobile
 ADDITIONAL_BUILD_PROPERTIES += ro.modversion=OpenEclair-v1.2.2
 ADDITIONAL_DEFAULT_PROPERTIES += persist.service.memctl_state=1
