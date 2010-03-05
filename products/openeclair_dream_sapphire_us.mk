eee2847a2c58b804e7a510117d7c75dedf9907a4
Wes Garner <wesgarner@gmail.com>
2010-03-04 23:36:04 -0600

    use camera drivers from Droid Eris

--- products/openeclair_dream_sapphire_us.mk
+++ products/openeclair_dream_sapphire_us.mk
@@ -37,6 +37,8 @@ JS_ENGINE=V8
 #WITH_JIT := true
 #PRODUCT_PROPERTY_OVERRIDES += dalvik.vm.execution-mode=int:fast
 
+USE_CAMERA_STUB := true
+
 # Used by BusyBox
 KERNEL_MODULES_DIR:=/system/lib/modules
 
@@ -91,7 +93,7 @@ PRODUCT_COPY_FILES += vendor/openeclair/proprietary/libhtc_ril.so:system/lib/lib
 PRODUCT_COPY_FILES += vendor/openeclair/proprietary/libspeech.so:system/lib/libspeech.so
 PRODUCT_COPY_FILES += vendor/openeclair/prebuilt/dream-sapphire/camera/libcameraservice.so:system/lib/libcameraservice.so
 PRODUCT_COPY_FILES += vendor/openeclair/prebuilt/dream-sapphire/camera/libcamera.so:system/lib/libcamera.so
-PRODUCT_COPY_FILES += vendor/openeclair/prebuilt/dream-sapphire/camera/libqcamera.so:system/lib/libqcamera.so
+PRODUCT_COPY_FILES += vendor/openeclair/prebuilt/dream-sapphire/camera/libqcamera.so:system/lib/liboemcamera.so
 PRODUCT_COPY_FILES += vendor/openeclair/prebuilt/dream-sapphire/sapphire-keypad.kcm.bin:system/etc/keychars/sapphire-keypad.kcm.bin
 PRODUCT_COPY_FILES += vendor/openeclair/prebuilt/dream-sapphire/sensors.sapphire.so:system/lib/hw/sensors.sapphire.so
 PRODUCT_COPY_FILES += vendor/openeclair/prebuilt/dream-sapphire/lib/libext2_blkid.so:system/lib/libext2_blkid.so
