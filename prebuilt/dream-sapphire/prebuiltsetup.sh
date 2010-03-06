42b581f7654b0f1b6a0d49053684f4371cf9f37b
Wes Garner <wesgarner@gmail.com>
2010-03-05 22:45:25 -0600

    added libopencore

--- prebuilt/dream-sapphire/prebuiltsetup.sh
+++ prebuilt/dream-sapphire/prebuiltsetup.sh
@@ -1,13 +1,13 @@
 for prebuilt in * ; do
- 	echo PRODUCT_COPY_FILES += vendor/openeclair/prebuilt/$prebuilt\:system/$prebuilt;
+ 	echo PRODUCT_COPY_FILES += vendor/openeclair/prebuilt/dream-sapphire/$prebuilt\:system/$prebuilt;
 done;
 for prebuilt in */* ; do
- 	echo PRODUCT_COPY_FILES += vendor/openeclair/prebuilt/$prebuilt\:system/$prebuilt;
+ 	echo PRODUCT_COPY_FILES += vendor/openeclair/prebuilt/dream-sapphire/$prebuilt\:system/$prebuilt;
 done;
 for prebuilt in */*/* ; do
- 	echo PRODUCT_COPY_FILES += vendor/openeclair/prebuilt/$prebuilt\:system/$prebuilt;
+ 	echo PRODUCT_COPY_FILES += vendor/openeclair/prebuilt/dream-sapphire/$prebuilt\:system/$prebuilt;
 done;
 for prebuilt in */*/*/* ; do
- 	echo PRODUCT_COPY_FILES += vendor/openeclair/prebuilt/$prebuilt\:system/$prebuilt;
+ 	echo PRODUCT_COPY_FILES += vendor/openeclair/prebuilt/dream-sapphire/$prebuilt\:system/$prebuilt;
 done;
 
