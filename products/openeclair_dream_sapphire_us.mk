7508199c99e2df7322babc608804fdf737ea873d
Wes Garner <wesgarner@gmail.com>
2010-03-19 09:58:57 -0500

    Revert "Do not build live wallpapers."
    This reverts commit 792e9617aa376967ea37a6b27fa124368109dd43.

--- products/openeclair_dream_sapphire_us.mk
+++ products/openeclair_dream_sapphire_us.mk
@@ -68,6 +68,10 @@ PRODUCT_PACKAGES += \
 	DeskClock \
 	Gallery3D \
 	android.software.live_wallpaper.xml \
+	LiveWallpapersPicker \
+	LiveWallpapers \
+	MagicSmokeWallpapers \
+	VisualizationWallpapers \
 	Launcher \
 	libRS \
 	librs_jni 
