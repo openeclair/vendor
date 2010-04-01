2783659a101679a811166fd6aaad0dbe5d3ee3cb
Wes Garner <wesgarner@gmail.com>
2010-04-01 18:34:49 -0500

    fix products

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -26,7 +26,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/openeclair_common.mk \
-    $(LOCAL_DIR)/openeclair_dream_sapphire.mk \
-    $(LOCAL_DIR)/openeclair_dream_sapphire32A.mk \
-    $(LOCAL_DIR)/openeclair_dream_sapphire32Anew.mk
+    $(LOCAL_DIR)/openeclair_common_us.mk \
+    $(LOCAL_DIR)/openeclair_dream_sapphire_us.mk \
+    $(LOCAL_DIR)/openeclair_dream_sapphire32A_us.mk \
+    $(LOCAL_DIR)/openeclair_dream_sapphire32Anew_us.mk
