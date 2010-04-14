2548ebc126571686152df11891750ed0eb0c72a3
ctso <ctsoyars@gmail.com>
2010-04-14 07:15:14 +0000

    Removed proprietary, renamed dream-sapphire to dream_sapphire

--- products/openeclair_dream_sapphire.mk
+++ products/openeclair_dream_sapphire.mk
@@ -5,10 +5,14 @@ PRODUCT_BRAND := htc
 PRODUCT_DEVICE := dream_sapphire
 PRODUCT_MODEL := T-Mobile MyTouch3G
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=EPE54B BUILD_DISPLAY_ID=EPE54B PRODUCT_NAME=passion BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.1-update1/ERE27/24178:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.1-update1 ERE27 24178 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=EPE54B BUILD_DISPLAY_ID=EPE54B BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.1-update1/ERE27/24178:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.1-update1 ERE27 24178 release-keys"
 
 PRODUCT_PACKAGES += Stk
 
 TARGET_KERNEL_CONFIG ?= wg_defconfig
+
+PRODUCT_COPY_FILES += \
+    vendor/openeclair/prebuilt/dream_sapphire/build.sapphire.prop:system/build.sapphire.prop \
+    vendor/openeclair/prebuilt/dream_sapphire/build.trout.prop:system/build.trout.prop
 
 include vendor/htc-open/dream_sapphire/device.mk
